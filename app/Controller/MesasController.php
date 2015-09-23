394a50bdd60e66809eed4c8e31ff87c58834380c
Roberto Gonzalez <rgonzalez@eluniversal.com>
2015-09-22 21:45:54 -0430

    Subida Inicial
    Subida Inicial

---
+++ app/Controller/MesasController.php
@@ -0,0 +1,131 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Mesas Controller
+ *
+ * @property Mesa $Mesa
+ * @property PaginatorComponent $Paginator
+ */
+class MesasController extends AppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+
+	public function isAuthorized($user)
+	{
+		if($user['role'] == 'user')
+		{
+			if(in_array($this->action, array('add', 'index', 'view', 'edit')))
+			{
+				return true;
+			}
+			else
+			{
+				if($this->Auth->user('id'))
+				{
+					$this->Session->setFlash('No puede acceder', 'default', array('class' => 'alert alert-danger'));
+					$this->redirect($this->Auth->redirect());
+				}
+			}
+		}
+		
+		return parent::isAuthorized($user);
+	}
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Mesa->recursive = 0;
+		$this->Paginator->settings = array('limit' => 5);
+		$this->set('mesas', $this->Paginator->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Mesa->exists($id)) {
+			throw new NotFoundException(__('Invalid mesa'));
+		}
+		$options = array('conditions' => array('Mesa.' . $this->Mesa->primaryKey => $id));
+		$this->set('mesa', $this->Mesa->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->Mesa->create();
+			if ($this->Mesa->save($this->request->data)) {
+				$this->Session->setFlash('The mesa has been saved.', 'default', array('class' => 'alert alert-success'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash('The mesa could not be saved. Please, try again.', 'default', array('class' => 'alert alert-danger'));
+			}
+		}
+		$meseros = $this->Mesa->Mesero->find('list');
+		$this->set(compact('meseros'));
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->Mesa->exists($id)) {
+			throw new NotFoundException(__('Invalid mesa'));
+		}
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Mesa->save($this->request->data)) {
+				$this->Session->setFlash('The mesa has been saved.', 'default', array('class' => 'alert alert-success'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash('The mesa could not be saved. Please, try again.', 'default', array('class' => 'alert alert-danger'));
+			}
+		} else {
+			$options = array('conditions' => array('Mesa.' . $this->Mesa->primaryKey => $id));
+			$this->request->data = $this->Mesa->find('first', $options);
+		}
+		$meseros = $this->Mesa->Mesero->find('list');
+		$this->set(compact('meseros'));
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Mesa->id = $id;
+		if (!$this->Mesa->exists()) {
+			throw new NotFoundException(__('Invalid mesa'));
+		}
+		$this->request->allowMethod('post', 'delete');
+		if ($this->Mesa->delete()) {
+			$this->Session->setFlash('The mesa has been deleted.', 'default', array('class' => 'alert alert-success'));
+		} else {
+			$this->Session->setFlash('The mesa could not be deleted. Please, try again.', 'default', array('class' => 'alert alert-danger'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
+}
